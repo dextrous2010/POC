4d173fac52d8aa5c1ee1f31db6574e18877f1ae6
dextrous2010 <dextrous2010@gmail.com>
2020-10-31 23:07:47 +0200

    Adding SpecFlowTests proj with implemented BDD using SpecFlow

---
+++ SpecFlowTests/Features/SiteSearching.feature
@@ -0,0 +1,37 @@
+﻿Feature: SiteSearching
+	In order to make sure the searching works fine
+	I would like to test some common scenarios
+
+@Title-Chrome
+Scenario: [Chrome] Title Contains Searched Word
+	Given the browser type is Chrome
+	And the search word is automation
+	And the search result link number to open is 1
+	When the search result link is opened
+	Then the title should contain searched world automation
+
+@Title-Firefox
+Scenario: [Firefox] Title Contains Searched Word
+	Given the browser type is Firefox
+	And the search word is automation
+	And the search result link number to open is 1
+	When the search result link is opened
+	Then the title should contain searched world automation
+
+@Domain-Chrome
+Scenario: [Chrome] Searched Results Contain Domain
+	Given the browser type is Chrome
+	And the search word is automation
+	And the search pages count is 5
+	And the search domain is testautomationday
+	When the search is done
+	Then the searched results should contain domain testautomationday
+
+@Domain-Firefox
+Scenario: [Firefox] Searched Results Contain Domain
+	Given the browser type is Firefox
+	And the search word is automation
+	And the search pages count is 5
+	And the search domain is testautomationday
+	When the search is done
+	Then the searched results should contain domain testautomationday
---
+++ SpecFlowTests/Steps/SiteSearchingStepDefinition.cs
@@ -0,0 +1,99 @@
+﻿using TechTalk.SpecFlow;
+using SitesTesting.Sites.Google;
+using SitesTesting.Helpers;
+using SitesTesting.Browsers;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace SpecFlowTests.Steps
+{
+    [Binding]
+    public sealed class SiteSearchingStepDefinition
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        private BrowserType BrowserType { get; set; }
+        private string SearchWord { get; set; }
+        private int LinkNumber { get; set; }
+        private string Title { get; set; }
+
+        private string SearchDomain { get; set; }
+        private int SearchPagesCount { get; set; }
+        private bool SearchedRresultsContainDomain { get; set; }
+
+
+        public SiteSearchingStepDefinition(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the browser type is (.*)")]
+        public void GivenTheBrowserTypeIs(BrowserType browserType)
+        {
+            BrowserType = browserType;
+        }
+
+        [Given("the search word is (.*)")]
+        public void GivenTheSearchWordIs(string searchWord)
+        {
+            SearchWord = searchWord;
+        }
+
+        [Given("the search result link number to open is (.*)")]
+        public void GivenTheSearchResultLinkNumberToOpenIs(int linkNumber)
+        {
+            LinkNumber = linkNumber;
+        }
+
+        [When("the search result link is opened")]
+        public void WhenTheSearchResultLinkIsOpened()
+        {
+            IWebDriver driver = TestHelper.InitializeBrowserInstance(BrowserType).Driver;
+            Title = new GoogleSearchWebPage(driver)
+                .GoToPage()
+                .Search(SearchWord)
+                .OpenSearchResultLink(LinkNumber)
+                .Title;
+
+            driver.Quit();
+        }
+
+        [Then("the title should contain searched world (.*)")]
+        public void ThenTheTitleShouldContainSearchedWorld(string searchWord)
+        {
+            Assert.That(Title.ToUpper().Contains(searchWord.ToUpper()), $"Title does not contain the searched word {searchWord}!");
+        }
+
+
+        [Given("the search domain is (.*)")]
+        public void GivenTheSearchDomainIs(string searchDomain)
+        {
+            SearchDomain = searchDomain;
+        }
+
+        [Given("the search pages count is (.*)")]
+        public void GivenTheSearchDomainIs(int searchPagesCount)
+        {
+            SearchPagesCount = searchPagesCount;
+        }
+
+        [When("the search is done")]
+        public void WhenTheSearchIsDone()
+        {
+            IWebDriver driver = TestHelper.InitializeBrowserInstance(BrowserType).Driver;
+
+            SearchedRresultsContainDomain = new GoogleSearchWebPage(driver)
+                .GoToPage()
+                .Search(SearchWord)
+                .SearchedResultsContainDomain(SearchDomain, SearchPagesCount);
+
+            driver.Quit();
+        }
+
+        [Then("the searched results should contain domain (.*)")]
+        public void ThenTheSearchedRresultsShouldContainDomain(string searchWord)
+        {
+            Assert.That(SearchedRresultsContainDomain, $"There is no the expected domain on the first {SearchPagesCount} pages!");
+        }
+    }
+}
